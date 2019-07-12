b16e449317b73a227623ea2dc885fb6496ad7bad
FunBoyProjects <emmanuelshadow711@gmail.com>
2019-07-12 09:58:21 +0000

    Importation de tout le projet

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float mouseX, mouseY;
+    public float maxUp, maxDown;
+    public float RotateSpeed;
+    public Transform target, Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X");
+        mouseY -= Input.GetAxis("Mouse Y");
+        mouseY = Mathf.Clamp(mouseY, maxUp, maxDown);
+
+        transform.LookAt(target);
+       
+    }
+    private void LateUpdate()
+    {
+        Vector3 desiredRotation = new Vector3(mouseY, mouseX, 0);
+
+        Vector3 smoothedRotation = Vector3.Lerp(target.rotation.eulerAngles, desiredRotation, RotateSpeed);
+        target.rotation = Quaternion.Euler(smoothedRotation);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed,sprintSpeed, jumpForce;
+    Vector3 MoveVelocity;
+    float mouseX, mouseY;
+    Rigidbody rb;
+    public bool grounded, canJump;
+
+   
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            MoveVelocity = transform.TransformDirection(Movement) * sprintSpeed * Time.deltaTime;
+
+        }
+        else
+        {
+            MoveVelocity = transform.TransformDirection(Movement) * speed * Time.deltaTime;
+
+        }
+
+        rb.MovePosition(transform.position +  MoveVelocity);
+
+        if (Input.GetKeyDown(KeyCode.C) && grounded)
+        {
+
+            rb.velocity += new Vector3(0,jumpForce);
+
+            grounded = false;
+
+
+        }
+
+        
+    }
+
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (!col.collider.gameObject.CompareTag("Obstacle"))
+        {
+            grounded = true;
+ 
+        }
+    }
+
+}
